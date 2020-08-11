99e7847b8bc63d673f41ad955f40b12b23afa80f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-08-11 21:38:12 +0900

    Show RepositoryListView

--- ios/prototype/prototype/SceneDelegate.swift
+++ ios/prototype/prototype/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-//            window.rootViewController = UIHostingController(rootView: RespositoryListView(viewModel: .init()))
+            window.rootViewController = UIHostingController(rootView: RepositoryListView(viewModel: .init()))
             self.window = window
             window.makeKeyAndVisible()
         }
--- ios/prototype/prototype/Views/RepositoryListView.swift
+++ ios/prototype/prototype/Views/RepositoryListView.swift
@@ -9,10 +9,15 @@ struct RepositoryListView: View {
     @ObservedObject var viewModel: RepositoryListViewModel
     
     var body: some View {
-        NavigationView {
+        return NavigationView {
             List(viewModel.repositories) { repository in
                 RepositoryListRow(repository: repository)
             }
+            .alert(isPresented: $viewModel.isErrorShown, content: { () -> Alert in
+                Alert(title: Text("Error"), message: Text(viewModel.errorMessage))
+            })
+            .navigationBarTitle(Text("Repositories"))
         }
+        .onAppear(perform: {self.viewModel.apply(.onAppear)})
     }
 }
