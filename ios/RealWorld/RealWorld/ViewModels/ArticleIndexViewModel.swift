820833f995cbacee159da8475e15a4e27a20dad0
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-08-12 20:59:28 +0900

    Create ArticleIndexView

---
+++ ios/RealWorld/RealWorld/ViewModels/ArticleIndexViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ArticleIndexViewModel.swift
+//  RealWorld
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+final class ArticleIndexViewModel: ObservableObject {
+    init(/* apiService */) {}
+}
---
+++ ios/RealWorld/RealWorld/Views/ArticleIndexView.swift
@@ -0,0 +1,14 @@
+//
+//  ArticleIndexView.swift
+//  RealWorld
+//
+
+import SwiftUI
+
+struct ArticleIndexView: View {
+    @ObservedObject var viewModel: ArticleIndexViewModel
+    
+    var body: some View {
+        return Text("ArticleIndexView")
+    }
+}
